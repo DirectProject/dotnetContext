e4ce5aecc57dcdbf8180d27bc3785d26f9b1f842
Joe Shook <joeshook@gmail.com>
2021-06-25 08:43:17 -0700

    Moving from MimeKitLight to MimeKit. Preparing to add Benchmarkdotnet...

---
+++ benchmarks/Health.Direct.Context.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Health.Direct.Context.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
